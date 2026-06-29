126dfa588d1c120d26e58f1347b051a1e0b57d60
faizan <faizan.faizy3@gmail.com>
2026-06-29 17:06:52 +0530

    Revert intentional bug in add()

--- src/main/java/com/practice/calculator/Calculator.java
+++ src/main/java/com/practice/calculator/Calculator.java
@@ -3,7 +3,7 @@ package com.practice.calculator;
 public class Calculator {
 
     public int add(int a, int b) {
-        return a + b + 1; // BUG: intentionally wrong, for testing purposes
+        return a + b;
     }
 
     public int subtract(int a, int b) {
