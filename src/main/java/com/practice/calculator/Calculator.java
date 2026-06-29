507d7ab6d6c7c371142c11af62a395fa90dfacfa
faizan <faizan.faizy3@gmail.com>
2026-06-29 16:53:02 +0530

    Intentianally broken add () to test the pipeline

--- src/main/java/com/practice/calculator/Calculator.java
+++ src/main/java/com/practice/calculator/Calculator.java
@@ -3,7 +3,7 @@ package com.practice.calculator;
 public class Calculator {
 
     public int add(int a, int b) {
-        return a + b;
+        return a + b + 1; // BUG: intentionally wrong, for testing purposes
     }
 
     public int subtract(int a, int b) {
